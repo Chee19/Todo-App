0b8731ffc4c12d8be6c62cbb54f4d67092a16ded
Chee22 <cheehong@sirkkaltd.com>
2023-07-22 15:41:13 +0100

    Initial project file

---
+++ api/views.py
@@ -0,0 +1,58 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Note
+from .serializers import NoteSerializer
+
+
+# Create your views here.
+
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        {
+            'Endpoint': '/notes',
+            'method': 'GET',
+            'body': None,
+            'description': 'Return an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Return a single note object'
+        },
+        {
+            'Endpoint': '/note/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Create new note with data sent in POST request'
+        },
+        {
+            'Endpoint': '/note/id/update',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Replace an existing notes with data sent in PUT request'
+        },
+        {
+            'Endpoint': '/note/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Delete an existing note'
+        },
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
+def get_notes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_note(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
